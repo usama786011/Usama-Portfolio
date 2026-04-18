59a5005250685a727a0fec047fe4876a52ec2a11
git config --global user.name(Usama) <usama78601@gmail.com>
2026-04-18 21:19:01 +0500

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,7 +37,7 @@ function NavBar() {
       <Container>
         <Navbar.Brand href="/" className="d-flex">
           {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
-          <h1 className="project-heading pb-4">
+          <h1 className="project-heading pb-0">
           <strong className="purple">UA</strong></h1>
         </Navbar.Brand>
         <Navbar.Toggle
