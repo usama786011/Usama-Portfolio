d89e8a75c0873df0c8b0e2a1a44c23e5e819daa1
USAMA ARSHAD <Usama786011.com>
2026-05-04 15:56:37 +0500

    Fix Netlify build warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,10 +10,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3></h3>
+          <h3>Designed and Developed by Usama Arshad</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} Usama Arshad</h3>
+          <h3>Copyright © {year} SQA Engineer</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
