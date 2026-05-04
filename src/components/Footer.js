96f8ac71920245a38414519087a8d0d5cf6416b6
USAMA ARSHAD <Usama786011.com>
2026-05-04 16:05:38 +0500

    check changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Usama Arshad</h3>
+          <h3>Designed and Developed by Usama Arshad</h3> 
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SQA Engineer</h3>
